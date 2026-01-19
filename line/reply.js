9d9a5246ac4bcc21a0f2409e000a8b89913e1d9c
ohmugi <zukky.8nohe@gmail.com>
2026-01-19 15:40:26 +0900

    Add replyText function for LINE messaging API

---
+++ line/reply.js
@@ -0,0 +1,16 @@
+//line/reply.js
+import fetch from "node-fetch";
+
+export async function replyText(replyToken, text) {
+  await fetch("https://api.line.me/v2/bot/message/reply", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      replyToken,
+      messages: [{ type: "text", text }],
+    }),
+  });
+}
