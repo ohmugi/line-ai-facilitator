39df5ddf8d8b462bea8bd72115d2e9b52ffcbab7
ohmugi <zukky.8nohe@gmail.com>
2026-01-24 22:25:31 +0900

    Update reply.js

--- src/line/reply.js
+++ src/line/reply.js
@@ -1,3 +1,20 @@
 export async function replyText(replyToken, text) {
-  console.log("[replyText mock]", text);
+  await axios.post(
+    "https://api.line.me/v2/bot/message/reply",
+    {
+      replyToken,
+      messages: [
+        {
+          type: "text",
+          text,
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
